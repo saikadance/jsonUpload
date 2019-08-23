2f15b0d56f926c0d35f36fff0124595fab062c65
SearchBird <764064271@qq.com>
2019-08-23 19:54:37 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add/rm <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	deleted:    README.md
    	deleted:    characterJson/Hoshiguma.json
    	deleted:    characterJson/SkyFire.json
    	deleted:    characterJson/Vigna.json
    	deleted:    characterJson/checkName.json
    	modified:   searchJson/cell-version-list.json
    	modified:   searchJson/duty-check.json
    	modified:   searchJson/range-check.json
    	modified:   searchJson/version-check.json
    	modified:   "word/\346\265\213\350\257\204\346\250\241\346\235\277(\345\221\275\345\220\215\345\222\214\344\270\212\344\274\240\346\227\240\345\205\263).docx"
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	Allcommit.sh
    	characterCellListJson/
    	characterJson/111.txt
    	characterJson/Shining[0001].json
    	characterJson/Shining[0002].json
    	nohup.out
    	searchJson-backup/
    	update.sh
    	"word/\346\265\213\350\257\204\346\250\241\346\235\277.zip"
    	"word/\347\224\237\346\210\220\351\225\277\345\233\276\346\250\241\346\235\2771-\347\272\242\350\261\206\346\265\213\350\257\204.jpg"
    
    no changes added to commit (use "git add" and/or "git commit -a")

---
+++ Allcommit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+ 
+#execute git command
+note=`git status`
+git status
+git add .
+git commit -am "$note"
+git pull --rebase
+git push -u origin master -f
+echo "================ Allcommit ================"
+sleep 1
---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#execute git command
+git status
+
+
+git branch
+
+
+git checkout remote branch
+git pull
+echo "================ UPDATE COMPLETE ================"
+sleep 1
